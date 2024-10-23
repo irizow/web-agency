9d19127e4371a64cf0c32aebfc5552becfecdb01
irizow <irisrossell@gmail.com>
2024-10-23 14:12:50 +0300

    redeploy to fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,13 @@ import styles from './App.module.css'
 import Header from './components/Header/Header'
 import Home from './components/Home'
 import { useState } from 'react';
-import Squiggle from './components/Squiggle/Squiggle';
 
 function App() {
   const [showForm, setShowForm] = useState(false);
 
   return (
 
-    <div>
+    <div className={styles.mindiv}>
     <Header setShowForm={setShowForm}/>
     <Home showForm={showForm} setShowForm={setShowForm} />
     </div>
