fb7338f6c165281c6cbb9949102e216cc6fec5a0
irizow <irisrossell@gmail.com>
2025-04-13 22:05:45 +0200

    change services text

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -22,131 +22,132 @@ export default function Services() {
         const {language} = useContext(LanguageContext);
 
 
-    const translations = {
-  en: {
-    title: 'SERVICES',
-    services: [
-      {
-        title: 'web design & development',
-        subtitle: 'we build websites that do more than just look good—they perform.',
-        text1: 'we fuse creativity with strategy, creating custom sites that engage your visitors and turn them into loyal customers.',
-        text2: 'all sites custom built using html, css, js and react.',
-        img: isSmall ? designVect : designImg,
-        alt: 'drawing of a laptop and a code editor',
-        id: 1,
-      },
-      {
-        title: 'logo & branding',
-        subtitle: 'your brand’s identity should be as unique as the business you’ve built.',
-        text1: 'we fuse creativity with strategy, creating custom sites that engage your visitors and turn them into loyal customers.',
-        text2: 'all sites custom built using html, css, js and react.',
-        img: isSmall ? brandingVect : brandingImg,
-        alt: 'drawing of design tablet',
-        id: 2,
-      },
-      {
-        title: 'content creation',
-        subtitle: 'captivating content is key to driving engagement and building your brand.',
-        text1: 'we build websites that do more than just look good—they perform.',
-        text2: 'we build websites that do more than just look good—they perform.',
-        img: isSmall ? contentVect : contentImg,
-        alt: 'drawing of a instagram picture of a cat',
-        id: 3,
-      },
-      {
-        title: 'web maintenance',
-        subtitle: 'your website is the face of your business, and it needs regular care to stay in top shape.',
-        text1: 'we build websites that do more than just look good—they perform.',
-        text2: 'we build websites that do more than just look good—they perform.',
-        img: maintenanceImg,
-        alt: 'drawing that suggest settings on websites',
-        id: 4,
-      },
-    ]
-  },
-  sp: {
-    title: 'SERVICIOS',
-    services: [
-      {
-        title: 'diseño y desarrollo web',
-        subtitle: 'creamos sitios que no solo se ven bien—también funcionan.',
-        text1: 'fusionamos creatividad con estrategia para crear sitios personalizados que cautivan a tus visitantes y los convierten en clientes fieles.',
-        text2: 'todos los sitios están hechos a medida con html, css, js y react.',
-        img: isSmall ? designVect : designImg,
-        alt: 'dibujo de un portátil y un editor de código',
-        id: 1,
-      },
-      {
-        title: 'logotipo e identidad de marca',
-        subtitle: 'la identidad de tu marca debe ser tan única como el negocio que has creado.',
-        text1: 'fusionamos creatividad con estrategia para crear sitios personalizados que cautivan a tus visitantes y los convierten en clientes fieles.',
-        text2: 'todos los sitios están hechos a medida con html, css, js y react.',
-        img: isSmall ? brandingVect : brandingImg,
-        alt: 'dibujo de una tableta de diseño',
-        id: 2,
-      },
-      {
-        title: 'creación de contenido',
-        subtitle: 'el contenido cautivador es clave para generar interacción y construir tu marca.',
-        text1: 'creamos sitios que no solo se ven bien—también funcionan.',
-        text2: 'creamos sitios que no solo se ven bien—también funcionan.',
-        img: isSmall ? contentVect : contentImg,
-        alt: 'dibujo de una foto de Instagram de un gato',
-        id: 3,
-      },
-      {
-        title: 'mantenimiento web',
-        subtitle: 'tu sitio web es la cara de tu negocio, y necesita cuidados regulares para mantenerse en forma.',
-        text1: 'creamos sitios que no solo se ven bien—también funcionan.',
-        text2: 'creamos sitios que no solo se ven bien—también funcionan.',
-        img: maintenanceImg,
-        alt: 'dibujo que sugiere configuraciones en sitios web',
-        id: 4,
-      },
-    ]
-  },
-  ca: {
-    title: 'SERVEIS',
-    services: [
-      {
-        title: 'disseny i desenvolupament web',
-        subtitle: 'creem webs que no només es veuen bé—també funcionen.',
-        text1: 'fusionem creativitat i estratègia per crear webs personalitzats que enganxin als visitants i els converteixin en clients fidels.',
-        text2: 'tots els llocs estan creats a mida amb html, css, js i react.',
-        img: isSmall ? designVect : designImg,
-        alt: 'dibuix d’un portàtil i un editor de codi',
-        id: 1,
-      },
-      {
-        title: 'logotip i identitat de marca',
-        subtitle: 'la identitat de la teva marca hauria de ser tan única com el negoci que has creat.',
-        text1: 'fusionem creativitat i estratègia per crear webs personalitzats que enganxin als visitants i els converteixin en clients fidels.',
-        text2: 'tots els llocs estan creats a mida amb html, css, js i react.',
-        img: isSmall ? brandingVect : brandingImg,
-        alt: 'dibuix d’una tauleta de disseny',
-        id: 2,
-      },
-      {
-        title: 'creació de contingut',
-        subtitle: 'el contingut captivador és clau per generar interacció i fer créixer la teva marca.',
-        text1: 'creem webs que no només es veuen bé—també funcionen.',
-        text2: 'creem webs que no només es veuen bé—també funcionen.',
-        img: isSmall ? contentVect : contentImg,
-        alt: 'dibuix d’una foto d’Instagram d’un gat',
-        id: 3,
-      },
-      {
-        title: 'manteniment web',
-        subtitle: 'el teu lloc web és la cara del teu negoci i necessita cures regulars per mantenir-se en forma.',
-        text1: 'creem webs que no només es veuen bé—també funcionen.',
-        text2: 'creem webs que no només es veuen bé—també funcionen.',
-        img: maintenanceImg,
-        alt: 'dibuix que suggereix configuració en llocs web',
-        id: 4,
-      },
-    ]
-  }
-};
+        const translations = {
+          en: {
+            title: 'SERVICES',
+            services: [
+              {
+                title: 'web design & development',
+                subtitle: 'we build websites that do more than just look good—they perform.',
+                text1: 'we fuse creativity with strategy, creating custom sites that engage your visitors and turn them into loyal customers.',
+                text2: 'all sites custom built using html, css, js and react.',
+                img: isSmall ? designVect : designImg,
+                alt: 'drawing of a laptop and a code editor',
+                id: 1,
+              },
+              {
+                title: 'logo & branding',
+                subtitle: 'your brand’s identity should be as unique as the business you’ve built.',
+                text1: 'we design logos and visual systems that reflect your core values and resonate with your target audience.',
+                text2: 'each branding package is crafted with strategy, clarity, and creative flair.',
+                img: isSmall ? brandingVect : brandingImg,
+                alt: 'drawing of design tablet',
+                id: 2,
+              },
+              {
+                title: 'content creation',
+                subtitle: 'captivating content is key to driving engagement and building your brand.',
+                text1: 'we craft compelling visuals and copy that spark interest and drive action across platforms.',
+                text2: 'from social media to websites, we help you tell your story with clarity and style.',
+                img: isSmall ? contentVect : contentImg,
+                alt: 'drawing of an instagram picture of a cat',
+                id: 3,
+              },
+              {
+                title: 'web maintenance',
+                subtitle: 'your website is the face of your business, and it needs regular care to stay in top shape.',
+                text1: 'we handle updates, backups, and security to keep your site running smoothly and safely.',
+                text2: 'think of us as your on-call digital support team.',
+                img: maintenanceImg,
+                alt: 'drawing that suggests settings on websites',
+                id: 4,
+              },
+            ]
+          },
+          sp: {
+            title: 'SERVICIOS',
+            services: [
+              {
+                title: 'diseño y desarrollo web',
+                subtitle: 'creamos sitios que no solo se ven bien—también funcionan.',
+                text1: 'fusionamos creatividad y estrategia para crear sitios personalizados que cautivan a tus visitantes y los convierten en clientes fieles.',
+                text2: 'todos los sitios están hechos a medida con html, css, js y react.',
+                img: isSmall ? designVect : designImg,
+                alt: 'dibujo de un portátil y un editor de código',
+                id: 1,
+              },
+              {
+                title: 'logotipo e identidad de marca',
+                subtitle: 'la identidad de tu marca debe ser tan única como el negocio que has creado.',
+                text1: 'diseñamos logotipos y sistemas visuales que reflejan tus valores y conectan con tu audiencia ideal.',
+                text2: 'cada paquete de branding se elabora con estrategia, claridad y un toque creativo.',
+                img: isSmall ? brandingVect : brandingImg,
+                alt: 'dibujo de una tableta de diseño',
+                id: 2,
+              },
+              {
+                title: 'creación de contenido',
+                subtitle: 'el contenido cautivador es clave para generar interacción y construir tu marca.',
+                text1: 'creamos visuales atractivos y textos que generan interés y provocan acción en todos los canales.',
+                text2: 'desde redes sociales hasta sitios web, te ayudamos a contar tu historia con claridad y estilo.',
+                img: isSmall ? contentVect : contentImg,
+                alt: 'dibujo de una foto de Instagram de un gato',
+                id: 3,
+              },
+              {
+                title: 'mantenimiento web',
+                subtitle: 'tu sitio web es la cara de tu negocio, y necesita cuidados regulares para mantenerse en forma.',
+                text1: 'nos encargamos de actualizaciones, copias de seguridad y seguridad para que tu sitio funcione sin problemas.',
+                text2: 'piensa en nosotros como tu equipo de soporte digital bajo demanda.',
+                img: maintenanceImg,
+                alt: 'dibujo que sugiere configuraciones en sitios web',
+                id: 4,
+              },
+            ]
+          },
+          ca: {
+            title: 'SERVEIS',
+            services: [
+              {
+                title: 'disseny i desenvolupament web',
+                subtitle: 'creem webs que no només es veuen bé—també funcionen.',
+                text1: 'fusionem creativitat i estratègia per crear webs personalitzats que captiven als visitants i els converteixen en clients fidels.',
+                text2: 'tots els llocs estan creats a mida amb html, css, js i react.',
+                img: isSmall ? designVect : designImg,
+                alt: 'dibuix d’un portàtil i un editor de codi',
+                id: 1,
+              },
+              {
+                title: 'logotip i identitat de marca',
+                subtitle: 'la identitat de la teva marca hauria de ser tan única com el negoci que has creat.',
+                text1: 'dissenyem logotips i sistemes visuals que reflecteixen els teus valors i connecten amb el teu públic objectiu.',
+                text2: 'cada paquet de marca està elaborat amb estratègia, claredat i creativitat.',
+                img: isSmall ? brandingVect : brandingImg,
+                alt: 'dibuix d’una tauleta de disseny',
+                id: 2,
+              },
+              {
+                title: 'creació de contingut',
+                subtitle: 'el contingut captivador és clau per generar interacció i fer créixer la teva marca.',
+                text1: 'creem visuals atractius i textos que generen interès i acció a totes les plataformes.',
+                text2: 'des de xarxes socials fins a webs, t’ajudem a explicar la teva història amb estil i claredat.',
+                img: isSmall ? contentVect : contentImg,
+                alt: 'dibuix d’una foto d’Instagram d’un gat',
+                id: 3,
+              },
+              {
+                title: 'manteniment web',
+                subtitle: 'el teu lloc web és la cara del teu negoci i necessita cures regulars per mantenir-se en forma.',
+                text1: 'ens encarreguem de les actualitzacions, còpies de seguretat i seguretat perquè el teu web funcioni de manera fluida i segura.',
+                text2: 'pensa en nosaltres com el teu equip digital de confiança.',
+                img: maintenanceImg,
+                alt: 'dibuix que suggereix configuració en llocs web',
+                id: 4,
+              },
+            ]
+          }
+        };
+        
 
     function ServicesCard({title, subtitle, text1, text2, img, alt, id, x, y}) {
         const [isOpen, setIsOpen] = useState(false);
